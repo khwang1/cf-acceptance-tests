41120c04638b4bb86d80591a337d20ba79d1a2fa
Anand Gaitonde and Dan Wendorf <agaitonde@pivotallabs.com>
2014-04-18 17:31:00 -0700

    wip

---
+++ exports.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export CONFIG=`pwd`/integration_config.json
+
+export API_ENDPOINT=http://api.174.129.252.37.xip.io
+
+export ADMIN_USER=admin
+export ADMIN_PASSWORD=admin
+export CF_USER=cats-user-osx
+export CF_USER_PASSWORD=cats-password
+export CF_ORG=cats-org-osx
+export CF_SPACE=cats-space-osx
+export CF_LOGIN_FLAGS="--skip-ssl-validation"
